2487ddf350c98fd7a686e341f951daf100d29df3
cvermand <charlottevermandel@gmail.com>
2018-01-28 16:29:09 +0100

    aa

--- gitme.sh
+++ gitme.sh
@@ -2,10 +2,9 @@
 git add *
 git status
 read -p "Are you sure you want to push?" -n 1 -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]
+if [[  $REPLY =~ ^[Yy]$ ]]
 then
-	echo "hello"
 	git commit -m "$1"
 	git push
 fi
-echo "byebye"
+echo "\nABORDED"
